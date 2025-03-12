9df49bd661d3732c6be4416a44c6ae1c992da073
Tony Shilati <anthonyshilati@gmail.com>
2025-03-12 16:30:17 -0500

    Added a function declaration for rodriquez fromula, the matrix exponential, and the matrix logarithm

--- src/include/open_chain_kinematics.hpp
+++ src/include/open_chain_kinematics.hpp
@@ -5,7 +5,9 @@
 #include <vector>
 
 namespace open_chain_kinematics {
-
+    Eigen::MatrixXd Rodriguez(const Eigen::VectorXd &omega, const float &theta);
+    Eigen::MatrixXd Matrix_Exponential(const Eigen::VectorXd &S, const float &theta);
+    Eigen::MatrixXd Matrix_Logarithm(const Eigen::MatrixXd &T);
     Eigen::VectorXd FKin_Space(const Eigen::MatrixXd M, const std::vector<Eigen::VectorXd> S_list, const std::vector<float> theta_list);
     Eigen::VectorXd FKin_Body(const Eigen::MatrixXd M, const std::vector<Eigen::VectorXd> B_list, const std::vector<float> theta_list);
 
--- src/open_chain_kinematics.cpp
+++ src/open_chain_kinematics.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include "include/open_chain_kinematics.hpp"
+#include <math.h>
 
 namespace open_chain_kinematics {
 
+    Eigen::MatrixXd Rodriguez(const Eigen::VectorXd &omega, const float &theta){
+        // Implements Rodriguez formula to calculate a rotation matrix from an axis and angle
+
+    }
+
+    Eigen::MatrixXd Matrix_Exponential(const Eigen::VectorXd &S, const float &theta){
+        // Calculates the matrix exponential of a rigid body motion
+
+    }
+
+    Eigen::MatrixXd Matrix_Logarithm(const Eigen::MatrixXd &T){
+        // Calculates the matrix logarithm of a rigid body motion
+
+    }
+
     Eigen::VectorXd FKin_Space(const Eigen::MatrixXd M, const std::vector<Eigen::VectorXd> S_list, const std::vector<float> theta_list) {
-        // Function definition for FKin_Space
-        // ...implementation code...
+        // Forward kinematics in the space frame
+
     }
 
     Eigen::VectorXd FKin_Body(const Eigen::MatrixXd M, const std::vector<Eigen::VectorXd> B_list, const std::vector<float> theta_list) {
         // Function definition for FKin_Body
-        // ...implementation code...
+        
     }
 
 } // namespace open_chain_kinematics
