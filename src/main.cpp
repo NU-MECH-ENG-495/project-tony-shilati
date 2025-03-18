4517d4c5b5ee2f040a0f43661bde9cf02968ed7e
tony-shilati <anthonyshilati2029@u.northwestern.edu>
2025-03-17 23:35:19 -0500

    Finger model forward kinematics (#5)
    * Changed link_lengths and joint_angles to an Eigen vector for compatibility with the kinematics library
    
    * Read through and fixed the MatrixLogarithm Code
    
    * Fixed matrixLogarithm to use tan instead of cot
    
    * Implemented Forward Kinematics in finger model
    
    * Fixed errors in finger model. It now compiles and runs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "include/finger_model.hpp"
-#include "include/render_finger.hpp"
 #include <Eigen/Dense>
 #include <math.h>
 #include <vector>
@@ -8,11 +7,14 @@
 int main() {
     std::cout << "Finger model program started." << std::endl;
 
-    // Create a vector of linlengths
-    std::vector<double> link_lengths = {0.046, 0.032, 0.025};
+    // Create a vector of link lengths
+    Eigen::VectorXd link_lengths(3); link_lengths << 0.046, 0.032, 0.025;
 
     // Create a vector of joint angles
-    std::vector<double> joint_angles = {0.0, 0.0, 0.0};
+    Eigen::VectorXd joint_angles(3); joint_angles << 0.0, 0.0, 0.0;
+
+    // Create a finger model
+    fm::finger_model finger(link_lengths, joint_angles);
 
     // Create a vector containing the fingers's home position screw axes
     Eigen::VectorXd S1(6); S1 << 0, 0, 1, 0, 0, 0;
@@ -30,17 +32,19 @@ int main() {
                                 0, 0, 1, 0,
                                 0, 0, 0, 1;
 
-    // Create a finger model
-    fm::finger_model finger(home_position_body_frame, home_position_screw_axes, link_lengths, joint_angles);
+    
+    // Set the finger's home position screw axes and home position in the body frame
+    finger.set_home_position_screw_axes_space(home_position_screw_axes);
+    finger.set_home_position_body_frame(home_position_body_frame);
+
+    
 
     std::cout << "Link lengths: ";
     for (const auto& length : finger.get_link_lengths()) {
         std::cout << length << " ";
     }
     std::cout << std::endl;
-
-    // Render the finger model
-    render_finger();
-
+    
+    // You can add code here to create and use the finger_model class
     return 0;
 }
